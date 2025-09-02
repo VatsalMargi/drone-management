e7e3b7e85482496e088b2866d896cbe6b26b1cc6
VatsalMargi <vatsalmargi2002@gmail.com>
2025-09-03 02:43:08 +0530

    imported redirect

--- src/app/actions.tsx
+++ src/app/actions.tsx
@@ -3,6 +3,7 @@
 
 import prisma from '@/lib/db';
 import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
 
 export async function addDrone(formData: FormData) {
   const name = formData.get('name') as string;
